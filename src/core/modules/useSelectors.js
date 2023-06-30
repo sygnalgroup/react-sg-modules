62a502c2d3eecc223811b5c20c3d705822ec2be0
mayander30 <mayander30@gmail.com>
2023-06-30 18:14:05 -0300

    addingnew param in use selectors

--- src/core/modules/useSelectors.js
+++ src/core/modules/useSelectors.js
@@ -2,8 +2,14 @@ import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { ReducersProvider } from '../contexts';
 
-export default (module) => {
+export default (module, prop = null) => {
   const { selectors } = useContext(ReducersProvider);
+
+  if (prop) {
+    return useSelector((state) => selectors[module].state(state)[prop]);
+  }
+
   const valueProp = useSelector((state) => selectors[module].state(state));
+
   return valueProp;
 };
